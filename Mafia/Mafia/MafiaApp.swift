ce3c51f322a7ae4152313210255037d5dcd54b46
Alexey Bitkov <alxsmrn@yandex.ru>
2021-09-22 21:40:45 +0300

    Initial commit

---
+++ Mafia/Mafia/MafiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MafiaApp.swift
+//  Mafia
+//
+//  Created by Alex on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MafiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
